df49d557e0b8522dee6d06cc689fb99f1c035f65
liangwei Zhang <40728744+liangweizhang@users.noreply.github.com>
2020-07-26 00:39:30 +0800

    Add files via upload

---
+++ NTfileStatistics.py
@@ -0,0 +1,43 @@
+import os
+def readNT(FileName):
+    fp=open(FileName,mode='r')
+    fileData=fp.name
+    line=fp.readline()
+    SumNum=0;
+    MAS_NUM=[0,0,0,0,0]
+    while line:
+        blankChar = 0
+        MAS=0;
+        for i in line:
+            if(blankChar == 5):
+                MAS=i
+                break
+            if (i==" "):
+                blankChar+=1
+        if(MAS=='0'):
+            MAS_NUM[0]+=1
+        elif(MAS=='1'):
+             MAS_NUM[1]+=1
+        elif(MAS=='3'):
+            MAS_NUM[2]+=1
+        elif(MAS=='5'):
+            MAS_NUM[3]+=1
+        elif(MAS=='7'):
+            MAS_NUM[4]+=1
+        SumNum+=1
+        line=fp.readline()
+    fp.close()
+    print("事例时间:",fp.name)
+    print(MAS_NUM,SumNum)
+    if((SumNum-MAS_NUM[0])>0):
+        print("M1事例比例： {:.2%}'".format(MAS_NUM[1]/(SumNum-MAS_NUM[0])))
+        print("M3事例比例： {:.2%}'".format(MAS_NUM[2]/(SumNum-MAS_NUM[0])))
+        print("M5事例比例： {:.2%}'".format(MAS_NUM[3]/(SumNum-MAS_NUM[0])))
+        print("M7事例比例： {:.2%}'".format(MAS_NUM[4]/(SumNum-MAS_NUM[0])))
+path=r'G:\MyPython\00_LHASSO_ENDA事例比例统计\ReadData'
+for i in os.walk(path):
+    path_tuple=i
+for name in path_tuple[2]:
+    txtpath=".\ReadData"+"\\"+name
+    readNT(txtpath)
+    
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# NTfileStatistics.py  
+
+统计NT文件中各个触发层次事例占的比例，不统计M0事例。
